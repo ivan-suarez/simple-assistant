1f08b9cf5017c50d9b2668671da183514d347a6f
ivan-suarez <ivansuarezcast@gmail.com>
2024-05-18 23:01:39 +0200

    Improve mouse hover detection

--- content_script.js
+++ content_script.js
@@ -1,15 +1,28 @@
 // content_script.js
+let currentTweet = null;
 
-document.addEventListener('mouseover', function(event) {
-    let tweetElement = event.target.closest('article');
+document.addEventListener('mouseover', function (event) {
+  let tweetElement = event.target.closest('article');
+  if (tweetElement && tweetElement !== currentTweet) {
+    // Update the current tweet
+    currentTweet = tweetElement;
+
     
+
     if (tweetElement) {
       let tweetText = tweetElement.querySelector('[lang]').innerText;
       console.log("Hovered Tweet Text: ", tweetText);
 
-      chrome.runtime.sendMessage({tweetText: tweetText}, function(response){
+      chrome.runtime.sendMessage({ tweetText: tweetText }, function (response) {
         console.log('Response from background', response);
+        chrome.runtime.sendMessage({ tweetText: response.explanation });
       })
+
     }
-  });
-  
+  }
+
+});
+
+document.addEventListener('mouseleave', function() {
+  currentTweet = null;
+});
--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
-document.getElementById('myButton').addEventListener('click', () => {
-    console.log('Button clicked');
-  });
-  
+// Listen for messages from the content script
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.tweetText) {
+      document.getElementById('tweetText').innerText = message.tweetText;
+  }
+});
